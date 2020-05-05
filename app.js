31cfbf02e8fc089b9a011f8597fc2a4878aad9d4
Izabela Petrovicova <i.petrovicova@gmail.com>
2020-05-04 20:54:44 -0700

    adding js file

---
+++ app.js
@@ -0,0 +1,12 @@
+// var message = "Hello Node!";
+
+// var sum = 5 + 3;
+
+// console.log(message);
+// console.log(sum);
+
+var commandLineArgs = process.argv;
+console.log(commandLineArgs);
+
+//test 1
+
