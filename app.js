86838710189529431ed6791b7b22015c5d50a693
Izabela Petrovicova <i.petrovicova@gmail.com>
2020-05-05 15:58:47 -0700

    generate webpage changes

--- app.js
+++ app.js
@@ -1,42 +1,15 @@
-// var message = "Hello Node!";
-
-// var sum = 5 + 3;
-
-// console.log(message);
-// console.log(sum);
-
-// process.argv[1];
-
-// var commandLineArgs = process.argv;
-// console.log(commandLineArgs);
+const fs = require("fs");
+const generatePage = require('./src/page-template.js');
 
 
-// const profileDataArgs = process.argv.slice(2, process.argv.length);
-// console.log(profileDataArgs);
+const profileDataArgs = process.argv.slice(2);
 
-// Notice the lack of parentheses around the `profileDataArr` parameter?
-// const printProfileData = profileDataArr => {
-//     // console.log(profileDataArr);
-//     for (let i=0; i < profileDataArr.length; i +=1) {
-//         console.log(profileDataArr[i]);
-//     }
-
-//     console.log("================");
-
-
-//     // profileDataArr.forEach(function(profileItem) {
-//     //     console.log(profileItem);
-//     // })
-
-//     profileDataArr.forEach(profileItem => console.log(profileItem));
-// }
-
-// printProfileData(profileDataArgs);
+const [name, github] = profileDataArgs;
 
 
 
-const generatePage = (userName, githubName) => `Name: ${userName}, Github: ${githubName}`;
-console.log(generatePage("Jane", "janehub"));
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
 
-
-
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
