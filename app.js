b1038da00f6fd72061ca4a0f141c3372fac37860
Izabela Petrovicova <i.petrovicova@gmail.com>
2020-05-04 21:54:53 -0700

    initial node code

--- app.js
+++ app.js
@@ -5,8 +5,32 @@
 // console.log(message);
 // console.log(sum);
 
-var commandLineArgs = process.argv;
-console.log(commandLineArgs);
+// process.argv[1];
 
-//test 1
+// var commandLineArgs = process.argv;
+// console.log(commandLineArgs);
+
+
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
+
+// Notice the lack of parentheses around the `profileDataArr` parameter?
+const printProfileData = profileDataArr => {
+    // console.log(profileDataArr);
+    for (let i=0; i < profileDataArr.length; i +=1) {
+        console.log(profileDataArr[i]);
+    }
+
+    console.log("================");
+
+
+    // profileDataArr.forEach(function(profileItem) {
+    //     console.log(profileItem);
+    // })
+
+    profileDataArr.forEach(profileItem => console.log(profileItem));
+}
+
+printProfileData(profileDataArgs);
+
 
